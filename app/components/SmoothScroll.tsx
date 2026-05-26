4ac2d72b628cc1f4e7ba8e08dcbdd3cae1bc290c
RafaDev29 <rosselgalarzarafael@gmail.com>
2026-05-25 23:33:03 -0500

    feat: implement smooth scrolling and scroll reveal components with GSAP

---
+++ app/components/SmoothScroll.tsx
@@ -0,0 +1,26 @@
+"use client";
+import { useEffect } from "react";
+import Lenis from "lenis";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+export default function SmoothScroll({ children }: { children: React.ReactNode }) {
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    const lenis = new Lenis({ lerp: 0.08 });
+
+    lenis.on("scroll", ScrollTrigger.update);
+
+    const tick = (time: number) => lenis.raf(time * 1000);
+    gsap.ticker.add(tick);
+    gsap.ticker.lagSmoothing(0);
+
+    return () => {
+      lenis.destroy();
+      gsap.ticker.remove(tick);
+    };
+  }, []);
+
+  return <>{children}</>;
+}
